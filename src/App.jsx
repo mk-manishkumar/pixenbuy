832244b0713317a31336b9ffcce11639576e2ae3
Manish Kumar <manish.login01@gmail.com>
2025-05-05 23:36:05 +0530

    added react context to save the auth details; auth error still prevails

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,10 @@ import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
-import Home from './components/pages/Home';
-import Category from './components/pages/Category';
-import { Toaster } from "@/components/ui/sonner"; 
+import Home from "./components/pages/Home";
+import Category from "./components/pages/Category";
+import { Toaster } from "@/components/ui/sonner";
+import { AuthProvider } from "./context/AuthContext";
 
 const appRouter = createBrowserRouter([
   {
@@ -27,10 +28,10 @@ const appRouter = createBrowserRouter([
 
 const App = () => {
   return (
-    <>
-      <Toaster richColors position="top-center" /> 
-      <RouterProvider router={appRouter} />
-    </>
+      <AuthProvider>
+        <Toaster richColors position="top-center" />
+        <RouterProvider router={appRouter} />
+      </AuthProvider>
   );
 };
 
